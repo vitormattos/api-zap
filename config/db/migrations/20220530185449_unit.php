d12bdd8b1e850944839ba3d05b959f0a438112a9
Vitor Mattos <vitor@php.rio>
2023-06-18 17:45:32 -0300

    Remove unused columns
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- config/db/migrations/20220530185449_unit.php
+++ config/db/migrations/20220530185449_unit.php
@@ -14,8 +14,6 @@ final class Unit extends AbstractMigration
             ->addColumn('bedrooms', 'integer', ['null' => true])
             ->addColumn('bathrooms', 'integer', ['null' => true])
             ->addColumn('total_areas', 'integer', ['null' => true])
-            ->addColumn('good', 'integer', ['null' => true])
-            ->addColumn('ignore', 'integer', ['null' => true])
             ->addColumn('created_at', 'datetime')
             ->addColumn('updated_at', 'datetime', ['null' => true])
             ->create();
